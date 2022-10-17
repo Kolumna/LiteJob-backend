298a364a599a3cfea0f24783ed4b4618d7c30e3d
K0lumna <michaljaniec12@wp.pl>
2022-10-17 22:32:17 +0200

    base

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.get("/api", (req, res) => {
     res.json({"Ulubione gry": ["ROCKET LEAGUE", "IT TAKES TWO", "PHASMOPHOBIA"]})
 })
 
-app.listen(5000, () => {console.log('ŁOOOOOO SERWER SIE URUCHAMIA!!!')})
+app.listen(process.env.PORT || 5000, () => {console.log('ŁOOOOOO SERWER SIE URUCHAMIA!!!')})
