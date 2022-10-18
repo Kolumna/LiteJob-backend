024e6e2782808ab0c9b6dd6ac0fbab3dfbce6578
K0lumna <michaljaniec12@wp.pl>
2022-10-18 19:52:37 +0200

    fix

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"Ulubione gry": ["ROCKET LEAGUE", "IT TAKES TWO", "PHASMOPHOBIA"]})
+    res.json({"oferta": ["truskawki", "magazyn", "sklep"]})
+})
+
+app.get("", (req, res) => {
+    res.send('<div style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;"><h1>LiteJob serwer działaaa :)</h1></div>')
 })
 
 app.listen(process.env.PORT || 5000, () => {console.log('ŁOOOOOO SERWER SIE URUCHAMIA!!!')})
