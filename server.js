a8bc5556b832b2198544f3b73a8a8f326740f5ec
K0lumna <michaljaniec12@wp.pl>
2022-10-20 11:42:49 +0200

    fixes

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://kolumna:37vMCiPnxaXDNXdP@litejobdb.jxw08wc.mongodb.net/?retryWrites=true&w=majority');
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
